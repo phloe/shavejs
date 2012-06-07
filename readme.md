58764aa06114c07567e1a22c1130182a4d90e897
Rasmus Fløe <rasmusfl0e@gmail.com>
2012-06-08 00:56:01 +0200

    use UMD pattern

--- readme.md
+++ readme.md
@@ -1,9 +1,67 @@
 # Shavejs
-#### Grooming your Mustache.
+## Grooming your Mustache.
 
-Shavejs is a layer ontop of Mustache that you can add helpers to.
+Shavejs is a layer ontop of Mustache.
 
-Shavejs exposes 2 methods; `shave.addHelper` and `shave.render`.
+
+### Constructor
+
+`shave` accepts an `options` argument with the following options:
+
+* `helpers` - (Object) An object containing helper functions for preprocessing data.
+* `data` - (Object) An object containing the data to populate the template.
+* `element` - (Element) An reference to an html element which will be the target of the `render` method.
+* `template` - (String) The url (or id) of the template to be used.
+* `ready` - (Function) A function that will be executed once the template is ready.  
+
+Returns an instance of `shave`.
+
+	shave({
+		template: "template.mustache",
+		data: {
+			foo: "bar"
+		},
+		element: document.getElementById("container"),
+		ready: function () {
+			this.render();
+		}
+	});
+
+### Methods
+
+`helper`
+
+* `name` - (String) The name to register the helper function by.
+* `func` - (Function) The helper function.
+
+`helpers`
+
+* `_helpers` - (Object) Containing helper functions; keys for names, values for functions.
+
+`data`
+
+* `data` - (Object) The data to populate the template.
+
+`element`
+
+* `element` - (Element) The element that will get targeted by the `render` method.
+
+`template`
+
+* `url` - (String) The url (or id) of the template.
+* `template` - (String) Optional. A mustache template.
+
+`ready`
+
+* `callback` - (Function) A function to be executed when the template is ready for use. Bound to `this`.
+ 
+
+`render`
+Preprocesses the data if needed and passes it through the template.
+If the `shave` instance has an element defined the resulting html is inserted into it and the instance is returned.
+Otherwise the html is returned. 
+
+### Manifest
 
 If you define a json manifest as a comment in your template shavejs will use that manifest to prepare your data:  
 
@@ -27,7 +85,3 @@ If you define a json manifest as a comment in your template shavejs will use tha
 	{{/articles}}
 
 In the above example `date|isodate` tells shavejs to parse the property through a helper registered as `isodate` (if it exists).
-
-	shave.addHelper("isodate", function (date) {
-		return date.toISOString(); 
-	})
--- shave.js
+++ shave.js
@@ -1,10 +1,189 @@
-define("shave", ["mustache"], function (_mustache) {
-
-	var cache = {},
+(function (root, factory) {
+	if (typeof exports === "object") {
+		module.exports = factory(require("mustache"));
+		root.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	}
+	else if (typeof define === "function" && define.amd) {
+		define("shave", ["mustache"], factory);
+	}
+	else {
+		root.shave = factory(root.Mustache);
+	}
+}(this, function (mustache) {
+	
+	var templates = {},
 		helpers = {},
 		isArray = Array.isArray || function (value) {
 			return value.constructor === Array;
 		};
+	
+	var Shave = function (options) {
+		this.context = {};
+		
+		if (options) {
+		
+			var props = ["helpers", "data", "element", "ready", "template"],
+				l = props.length,
+				prop;
+			
+			for (var i = 0; i < l; i++) {
+				prop = props[i];
+				if (prop in options) {
+					this[prop](options[prop]);
+				}
+			}
+			
+		}
+		
+		return this;
+	};
+	
+	Shave.prototype = {
+		
+		template: function (url, template) {
+			
+			if (!url) {
+				return this.context.template;
+			}
+			
+			if (template && !(url in templates)) {
+				templates[url] = template;
+			}
+			
+			if (url in templates) {
+				var definition = templates[url];
+				this.context.template = definition.template;
+				this.context.manifest = definition.manifest;
+				
+				if (this.context.ready) {
+					this.context.ready.call(self);
+				}
+			}
+			else {
+				
+				// read template into cache
+				// parse manifest from json header
+				
+				var self = this,
+					xhr = new XMLHttpRequest();
+				xhr.open("GET", url, true);
+				xhr.onload = function () {
+					var manifest, template,
+						match = xhr.responseText.match(/^(?:\{\{!([\s\S]*?|)!\}\})?([\s\S]*|)$/);
+						
+					template = match[2];
+					manifest = match[1] || null;
+					
+					if (manifest) {
+						manifest = JSON.parse(manifest);
+					}
+					
+					templates[url] = {
+						template: template,
+						manifest: manifest
+					};
+					
+					self.context.template = template;
+					self.context.manifest = manifest;
+					
+					if (self.context.ready) {
+						self.context.ready.call(self);
+					}
+					
+				};
+				xhr.send(null);
+				
+			}
+			
+			return this;
+			
+		},
+		
+		ready: function (callback) {
+			
+			if (!callback) {
+				return this.context.ready || null;
+			}
+			
+			this.context.ready = callback;
+			
+			return this;
+			
+		},
+		
+		element: function (element) {
+			
+			if (!element) {
+				return this.context.element || null;	
+			}
+			
+			this.context.element = element;
+			
+			return this;
+			
+		},
+		
+		data: function (data) {
+			
+			if (!data) {
+				return this.context.data || null;
+			}
+			
+			this.context.data = data;
+			
+			return this;
+		},
+		
+		helper: function (name, func) {
+			
+			if (!func) {
+				return helpers[name];
+			}
+			
+			helpers[name] = func;
+			
+			return this;
+		},
+		
+		helpers: function (_helpers) {
+			
+			if (!helpers) {
+				return helpers;
+			}
+			
+			for (var name in _helpers) {
+				helpers[name] = _helpers[name];
+			}
+			
+			return this;
+			
+		},
+		
+		render: function () {
+			
+			var manifest = this.context.manifest,
+				data = this.context.data,
+				template = this.context.template,
+				output = (manifest) ? preprocess(manifest, data) : data,
+				html = mustache.to_html(template, output);
+			
+			if (this.context.element) {
+				this.context.element.innerHTML = html;
+				return this;
+			}
+			
+			return html;
+			
+		},
+		
+		sort: function () {
+			
+			// todo
+			
+		}
+		
+		
+	};
 	
 	function preprocess (manifest, input) {
 		var output;
@@ -39,60 +218,8 @@ define("shave", ["mustache"], function (_mustache) {
 		return output;
 	}
 	
-	function returnHTML (callback, data, template, manifest) {
-		
-		// remap data?
-		
-		// preprocess data according to manifest
-		
-		var output = (manifest) ? preprocess(manifest, data) : data;
-		
-		callback(Mustache.to_html(template, output));
-		
-	}
-	
-	function render (url, data, callback) {
-		
-		if (url in cache) {
-			var definition = cache[url];
-			returnHTML(callback, data, definition.template, definition.manifest);
-		}
-		else {
-			
-			// read template into cache
-			// parse manifest from json header
-			
-			require(["text!" + url], function (response) {	
-				var manifest, template,
-					match = response.match(/^(?:\{\{!([\s\S]*?|)!\}\})?([\s\S]*|)$/);
-					
-				template = match[2];
-				manifest = match[1] || null;
-				
-				if (manifest) {
-					manifest = JSON.parse(manifest);
-				}
-				
-				cache[url] = {
-					template: template,
-					manifest: manifest
-				};
-				
-				returnHTML(callback, data, template, manifest);
-			});
-		}
-	}
-	
-	function addHelper (id, func) {
-		helpers[id] = func;
-	}
-
-	return {
-		
-		render: render,
-		
-		addHelper: addHelper
-		
+	return function (options) {
+		return new Shave(options || null); 
 	};
 	
-});
+}));
